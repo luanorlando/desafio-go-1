8d14d55229a84d3a4a2ac099e83f78ded02cc927
luanorlando <orlandoluan@hotmail.com>
2025-12-07 20:09:33 -0300

    feat(main.go): add dollsExchange rate request

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	fetchDolls()
+}
+
+func fetchDollsExchangeRate() {
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	var url string = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bodyBytes))
 }
