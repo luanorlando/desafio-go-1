821436792143813fb6011134737d615674a53a54
luanorlando <orlandoluan@hotmail.com>
2025-12-07 21:15:28 -0300

    feat: add server mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luanorlando/desafio-go-1
+
+go 1.25.2
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/luanorlando/desafio-go-1/server"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	server.FetchDollsExchangeRate()
 }
